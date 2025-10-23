ce9dcda285fffeb92db2cb7a34c80f0d4e8ce29c
caio14333 <caiosantiago14022010@gmail.com>
2025-10-23 14:01:34 -0300

    Questoes

--- questoes.js
+++ questoes.js
@@ -56,7 +56,7 @@ alert(`Você ganha aproximadamente ${resul} salarios minimos`)
 // 6 - Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.
 
 let valor = parseFloat(prompt("Digite um valor"));
-let resul2 = valor * 1.05
+let resul2 = valor*1.05
 alert(`O valor com um reajuste de 5% é ${resul2}`)
 
 // 7 - Faça um algoritmo que leia dois valores booleanos (lógicos) e determine se ambos são VERDADEIRO ou FALSO.
@@ -70,8 +70,12 @@ if (bool1 && bool2) {
 } else {alert(`Um é falso e outro é verdadeiro`)};
 
 // 8 - Faça um algoritmo que leia três valores inteiros diferentes e imprima na tela os valores em ordem decrescente.
-
-
+let numes = [];
+let n1 = parseFloat(prompt("Digite um valor"));
+let n2 = parseFloat(prompt("Digite um valor"));
+let n3 = parseFloat(prompt("Digite um valor"));
+let ordem = numes.sort((a, b) => b - a);
+alert(`A ordem decrescente é ${ordem}`)
 
 //9 - Faça um algoritmo que calcule o IMC (Índice de Massa Corporal) de uma pessoa, leia o seu peso e sua altura e imprima na tela sua condição de acordo com a tabela abaixo:
 // Fórmula do IMC = peso / (altura) ²
@@ -83,9 +87,84 @@ if (bool1 && bool2) {
 // Entre 35,0 e 39,9 | Obesidade grau II (severa)
 // Maior ou igual a 40 | Obesidade grau III (mórbida)
 
+let peso = parseFloat(prompt("Digite seu peso"));
+let altura = parseFloat(prompt("Digite seu peso"));
+let imc = peso / (altura)**2;
+if (imc > 18.5) {
+    alert(`Abaixo do peso`)
+} else if (24.9 > imc > 18.6) {
+    alert(`Peso ideal`)
+} else if (25 > imc > 29.9) {
+    alert(`Levemente acima do peso`)
+} else if (30 > imc > 34.9) {
+    alert(`Obesidade grau I`)
+} else if (35 > imc > 39.9) {
+    alert(`Obesidade grau II`)
+} else {
+    alert(`Obesidade grau III`)
+};
 
 // 10 - Faça um algoritmo que leia três notas obtidas por um aluno, e imprima na tela a média das notas.
+// 11 - Faça um algoritmo que leia quatro notas obtidas por um aluno, calcule a média das nota obtidas, imprima na tela o nome do aluno e se o aluno foi aprovado ou reprovado. Para o aluno ser considerado aprovado sua média final deve ser maior ou igual a 7.
+
+11 - Faça um algoritmo que leia quatro notas obtidas por um aluno, calcule a média das nota obtidas, imprima na tela o nome do aluno e 
+
+ se o aluno foi aprovado ou reprovado. Para o aluno ser considerado aprovado sua média final deve ser maior ou igual a 7.
 
  
 
-// 11 - Faça um algoritmo que leia quatro notas obtidas por um aluno, calcule a média das nota obtidas, imprima na tela o nome do aluno e se o aluno foi aprovado ou reprovado. Para o aluno ser considerado aprovado sua média final deve ser maior ou igual a 7.
+ 12 - Faça um algoritmo que leia o valor de um produto e determine o valor que deve ser pago, conforme a escolha da forma de pagamento
+
+ pelo comprador e imprima na tela o valor final do produto a ser pago. Utilize os códigos da tabela de condições de pagamento para efetuar o cálculo adequado.
+
+ 
+
+ Tabela de Código de Condições de Pagamento
+
+ 
+
+ 1 - À Vista em Dinheiro ou Pix, recebe 15% de desconto
+
+ 2 - À Vista no cartão de crédito, recebe 10% de desconto
+
+ 3 - Parcelado no cartão em duas vezes, preço normal do produto sem juros
+
+ 4 - Parcelado no cartão em três vezes ou mais, preço normal do produto mais juros de 10%
+
+ 
+
+ 13 - Faça algoritmo que leia o nome e a idade de uma peso e imprima na tela o nome da pessoa e se ela é maior ou menor de idade. 
+
+14 - Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o valor de B por A e imprima na tela os valores.
+
+15 - Faça um algoritmo que leia o ano em que uma pessoa nasceu, imprima na tela quantos anos, meses e dias essa pessoa ja viveu. Leve em 
+
+consideração o ano com 365 dias e o mês com 30 dias.
+
+(Ex: 5 anos, 2 meses e 15 dias de vida)
+
+16 - Faça um algoritmo que leia três valores que representam os três lados de um triângulo e verifique se são válidos, determine se o triângulo é 
+
+equilátero, isósceles ou escaleno.
+
+17 - Faça um algoritmo que leia uma temperatura em Fahrenheit e calcule a temperatura correspondente em grau Celsius. Imprima na tela as duas temperaturas.
+
+Fórmula: C = (5 * ( F-32) / 9)
+
+18 - Francisco tem 1,50m e cresce 2 centímetros por ano, enquanto Sara tem 1,10m e cresce 3 centímetros por ano. Faça um algoritmo que calcule e imprima na tela em quantos anos serão necessários para que Francisco seja maior que Sara.
+
+19 - Faça um algoritmo que imprima na tela a tabuada de 1 até 10.
+
+20 - Faça um algoritmo que receba um valor inteiro e imprima na tela a sua tabuada.
+
+21 - Faça um algoritmo que mostre um valor aleatório entre 0 e 100.
+
+22 - Faça um algoritmo que leia dois valores inteiros A e B, imprima na tela o quociente e o resto da divisão inteira entre eles.
+
+21 - Faça um algoritmo que efetue o cálculo do salário líquido de um professor. As informações fornecidas serão: valor da hora aula, número de aulas lecionadas no mês e percentual de desconto do INSS. Imprima na tela o salário líquido final.
+
+22 - Faça um algoritmo que calcule a quantidade de litros de combustível gastos em uma viagem, sabendo que o carro faz 12km com um litro. Deve-se fornecer ao usuário o tempo que será gasto na viagem a sua velocidade média, distância percorrida e a quantidade de litros utilizados para fazer a viagem.
+
+Fórmula: distância = tempo x velocidade.
+
+            litros usados = distância / 12.
